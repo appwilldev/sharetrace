96c119c083631f28212f4616a37b9349024bb1dc
liyong <liyongjie1982@gmail.com>
2016-02-26 14:08:55 +0800

    fix stats appid

--- web/js/utils.js
+++ web/js/utils.js
@@ -18,3 +18,36 @@ function clone(cond) {
         return cond;
     }
 }
+
+
+function isEmpty(obj) {
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
+
+
+$.extend({                                                                              getUrlVars: function(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); 
+        for(var i = 0; i < hashes.length; i++){
+            hash = hashes[i].split('=');
+            //vars.push(hash[0]); 
+            vars[hash[0]] = hash[1];
+        } 
+        return vars;
+    },
+}); 

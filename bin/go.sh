a2d4260f635c9585f8baa8daa3f385cb691f88d0
liyong <liyongjie1982@gmail.com>
2016-03-03 15:31:46 +0800

    fix shell

--- bin/go.sh
+++ bin/go.sh
@@ -9,4 +9,5 @@ OUT=st_linux_amd64
 #GOOS=linux go build -ldflags "-X main.date=$x -X main.rev=$y" -o $OUT
 GOOS=linux  GOARCH=amd64 go build -o $OUT
 
-scp $OUT sharetrace@beijing6.appdao.com:~/sharetrace
+OUT2=st_linux_amd64_new
+scp $OUT sharetrace@beijing6.appdao.com:~/sharetrace/$OUT2

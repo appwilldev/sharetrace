77899d024360879bb2ebeb94f1c26d5a6ec6cae3
catlovemouse <liyongjie1982@gmail.com>
2016-03-08 14:03:41 +0800

    add gotoAppStore func for js file

--- web/js/sharetrace.js
+++ web/js/sharetrace.js
@@ -1,10 +1,17 @@
 function set_stiframe(){
     var iframe = document.createElement('iframe');
-    console.log(iframe);
     var stiframe_url="http://st.apptao.com/1/st/webbeacon?share_url=" + encodeURIComponent(window.location);
     iframe.src = stiframe_url;
-    console.log(stiframe_url);
     document.body.appendChild(iframe);
 }
 
 set_stiframe()
+
+
+function gotoAppStore(buttonid){
+    var iframe = document.createElement('iframe');
+    alert(buttonid);
+    var stiframe_url="http://st.apptao.com/1/st/webbeaconbutton?buttonid=" + buttonid ;
+    iframe.src = stiframe_url;
+    document.body.appendChild(iframe);
+}
